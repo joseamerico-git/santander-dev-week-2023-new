14e51d9d49da1a55c8ca0d54b975209bc8e3bb94
joseamerico-gt <doichejunior@gmail.com>
2023-10-18 00:12:06 -0300

    BugFix Cors

--- src/main/java/doichejunior/Application.java
+++ src/main/java/doichejunior/Application.java
@@ -1,8 +1,12 @@
 package doichejunior;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//definindo o cors
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
